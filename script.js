036fc1a4243f2ab12645bbc3213ad47b7ce405e4
qweatr <tim.rakhimov25@gmail.com>
2025-08-21 16:47:37 -0700

    dz

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const sections = document.querySelectorAll('section');
+const sections = Array.from(document.querySelectorAll('section'));
 const buttons = document.querySelectorAll('.header__card');
+let activeButton = document.querySelector('.active')
 
 function handleScroll() {
-    const position = window.scrollY
-    let targetSection = sections[null]
+    const position = window.scrollY + 1
+    let targetSection = null
     for (let section of sections) {
         const sectionTop = section.offsetTop
         const sectionBottom = sectionTop + section.offsetHeight;
@@ -12,6 +13,11 @@ function handleScroll() {
             break;
         }
     }
+    const index = sections.indexOf(targetSection);
+    const targetButton = buttons[index];
+    activeButton.classList.remove('active');
+    targetButton.classList.add('active');
+    activeButton = targetButton;
 }
 
 window.addEventListener('scroll', handleScroll)
