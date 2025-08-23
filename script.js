f790e0fcb559adbbbe79ab0e306db0142a940211
qweatr <tim.rakhimov25@gmail.com>
2025-08-22 17:38:22 -0700

    last

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const sections = Array.from(document.querySelectorAll('section'));
 const buttons = document.querySelectorAll('.header__card');
 let activeButton = document.querySelector('.active')
 const button = document.querySelector('.mobile__button')
+let header__cards = document.querySelector('.header__cards')
+const icon = button.querySelector('img')
 
 function toogleMenu() {
-    console.log('Hello')
+    header__cards.classList.toggle('opened')
+    if (header__cards.classList.contains('opened')) {
+        icon.src = 'img/Copy_of_close_menu.png'
+    }
+    else {icon.src = 'img/mobile_menu.png'}
 }
 function handleScroll() {
     const position = window.scrollY + 1
